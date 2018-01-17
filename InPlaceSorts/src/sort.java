6b93034cb38c029bc321e5d95e798c1d35f9c6ff
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-01-17 09:24:12 -0500

    zxdcfvsdf

---
+++ InPlaceSorts/src/sort.java
@@ -0,0 +1,45 @@
+
+public class sort {
+	
+	int[] list1 = {3,5,4,2,6,1,8};
+	int[] double[] list2 = {3.0,5.5,4.3,2.7,6.3,1.3,8.7};
+	int[] String[] list1 = {3,5,4,2,6,1,8};
+	
+	
+	public static void insertionSort(int[] list1)
+	{
+		
+	}
+	
+	public static void selectionSort(double[] list2)
+	{
+		
+	}
+	
+	public static void bubbleSort(String[] list1)
+	{
+		
+	}
+	
+	public static void insertionSort(int[]arr, int index1, int index2)
+	{
+		int temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+	
+	public static void selectionSort(int[]arr, int index1, int index2)
+	{
+		int temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+	
+	public static void bubbleSort(int[]arr, int index1, int index2)
+	{
+		int temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+	
+}
